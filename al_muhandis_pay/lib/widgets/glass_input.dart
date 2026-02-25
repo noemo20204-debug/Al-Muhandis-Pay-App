b141a092d71ffc8b57b47a4e4b84e469888b49a0
Buraa <buraa@al-muhandis.com>
2026-02-25 15:29:39 +0000

    Fix: Synchronize Widget parameters and fix border syntax

--- al_muhandis_pay/lib/widgets/glass_input.dart
+++ al_muhandis_pay/lib/widgets/glass_input.dart
@@ -1,78 +1,52 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../core/elite_theme.dart';
 
-class GlassInput extends StatefulWidget {
+class GlassInput extends StatelessWidget {
   final TextEditingController controller;
   final String label;
   final IconData icon;
   final bool isPassword;
+  final bool obscureText;
+  final VoidCallback? onTogglePassword;
   final TextInputType keyboardType;
 
   const GlassInput({
-    super.key, required this.controller, required this.label,
-    required this.icon, this.isPassword = false, this.keyboardType = TextInputType.text,
+    super.key,
+    required this.controller,
+    required this.label,
+    required this.icon,
+    this.isPassword = false,
+    this.obscureText = false,
+    this.onTogglePassword,
+    this.keyboardType = TextInputType.text,
   });
 
   @override
-  State<GlassInput> createState() => _GlassInputState();
-}
-
-class _GlassInputState extends State<GlassInput> {
-  final FocusNode _focusNode = FocusNode();
-  bool _isFocused = false;
-  bool _obscure = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _focusNode.addListener(() {
-      setState(() => _isFocused = _focusNode.hasFocus);
-    });
-  }
-
-  @override
-  void dispose() {
-    _focusNode.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOutCubic,
+    return Container(
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        // التوهج الذهبي يظهر فقط عند الضغط على الحقل
-        boxShadow: _isFocused ? EliteShadows.neonGold : [],
+        color: Colors.white.withOpacity(0.05),
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: EliteColors.glassBorderLight),
       ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-          child: TextFormField(
-            controller: widget.controller,
-            focusNode: _focusNode,
-            obscureText: widget.isPassword ? _obscure : false,
-            keyboardType: widget.keyboardType,
-            style: const TextStyle(color: Colors.white, fontSize: 18, letterSpacing: 1.2),
-            decoration: InputDecoration(
-              labelText: widget.label,
-              labelStyle: GoogleFonts.cairo(color: _isFocused ? EliteColors.goldLight : Colors.grey.shade400, fontWeight: _isFocused ? FontWeight.bold : FontWeight.normal),
-              prefixIcon: Icon(widget.icon, color: _isFocused ? EliteColors.goldLight : EliteColors.goldPrimary),
-              suffixIcon: widget.isPassword ? IconButton(
-                icon: Icon(_obscure ? Icons.visibility : Icons.visibility_off, color: Colors.grey),
-                onPressed: () => setState(() => _obscure = !_obscure),
-              ) : null,
-              filled: true,
-              fillColor: EliteColors.glassFill,
-              border: OutlineInputBorder(borderRadius: BorderRadius.circular(16), borderSide: BorderSide.none),
-              focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(16), borderSide: const BorderSide(color: EliteColors.goldLight, width: 2)),
-              enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(16), borderSide: const BorderSide(color: EliteColors.glassBorderDark, width: 1)),
-            ),
-          ),
+      child: TextField(
+        controller: controller,
+        obscureText: isPassword ? obscureText : false,
+        keyboardType: keyboardType,
+        style: GoogleFonts.cairo(color: Colors.white),
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: GoogleFonts.cairo(color: Colors.grey),
+          prefixIcon: Icon(icon, color: EliteColors.goldPrimary),
+          suffixIcon: isPassword 
+            ? IconButton(
+                icon: Icon(obscureText ? Icons.visibility : Icons.visibility_off, color: Colors.grey),
+                onPressed: onTogglePassword,
+              )
+            : null,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
         ),
       ),
     );

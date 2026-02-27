913356c444daa389db707d05e136fdef89a30cdb
Buraa <buraa@al-muhandis.com>
2026-02-27 07:09:51 +0000

    🔒 إصلاح نهائي: تأمين بروتوكول الاتصال بالخزنة المركزية وتفعيل الـ Headers

--- al_muhandis_pay/macos/Flutter/GeneratedPluginRegistrant.swift
+++ al_muhandis_pay/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_secure_storage_darwin
 import gal
 import local_auth_darwin
 import share_plus
 import shared_preferences_foundation
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }

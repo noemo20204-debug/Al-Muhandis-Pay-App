ae30a5d934db196dc63697b2253802ae7d64e1a4
Buraa <buraa@al-muhandis.com>
2026-02-27 01:19:38 +0000

    Security: Deploy App Lifecycle Observer & Error 426 Kill-Switch

--- al_muhandis_pay/lib/main.dart
+++ al_muhandis_pay/lib/main.dart
@@ -2,17 +2,52 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'core/elite_theme.dart';
 import 'screens/splash_gate.dart';
+import 'services/api_engine.dart';
+
+// 🗝️ مفتاح الملاحة السيادي: يسمح لنا بالتحكم بالشاشات من أي مكان في التطبيق
+final GlobalKey<NavigatorState> globalNavigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  // ربط المفتاح بمحرك الاتصال قبل بدء تشغيل التطبيق
+  ApiEngine().setNavigatorKey(globalNavigatorKey);
   runApp(const AlMuhandisEliteApp());
 }
 
-class AlMuhandisEliteApp extends StatelessWidget {
+class AlMuhandisEliteApp extends StatefulWidget {
   const AlMuhandisEliteApp({super.key});
+
+  @override
+  State<AlMuhandisEliteApp> createState() => _AlMuhandisEliteAppState();
+}
+
+// إضافة مراقب حالة الهاتف (WidgetsBindingObserver)
+class _AlMuhandisEliteAppState extends State<AlMuhandisEliteApp> with WidgetsBindingObserver {
+  
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  // 📡 رادار العودة: يعمل فوراً عندما يفتح العميل التطبيق من الخلفية
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      ApiEngine().pingForVersionCheck();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: globalNavigatorKey, // زرع المفتاح هنا
       debugShowCheckedModeBanner: false,
       title: 'Al-Muhandis Pay Elite',
       theme: ThemeData(
